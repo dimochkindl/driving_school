72147a48847c8ace9fecae639e996413c05e92f5
dimochkindl <91985043+dimochkindl@users.noreply.github.com>
2024-02-14 03:57:46 +0300

    norm yml

---
+++ project/src/test/java/app/v1/V1ApplicationTest.java
@@ -0,0 +1,13 @@
+package app.v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V1ApplicationTest {
+
+    @Test
+    void contextLoads() {
+        // Тест успешен, если контекст приложения загружается без ошибок
+    }
+}
