b582f3ca21c19e6b75b50886da3289233c047189
dimochkindl <91985043+dimochkindl@users.noreply.github.com>
2024-02-17 18:48:50 +0300

    norm yml

--- project/src/main/java/app/v1/repositories/dao/StudentDAO.java
+++ project/src/main/java/app/v1/repositories/dao/StudentDAO.java
@@ -1,13 +1,13 @@
 package app.v1.repositories.dao;
 
-import app.v1.dto.StudentDTO;
 import app.v1.entities.Student;
 import app.v1.repositories.BaseRepository;
 
 import java.util.List;
 
 public interface StudentDAO extends BaseRepository<Student> {
-    StudentDTO getBySurname(String surname);
+    Student getBySurname(String surname);
+
     List<Integer> getTheoryGrades(Long id);
 
     List<Integer> getExamResults(Long id);
