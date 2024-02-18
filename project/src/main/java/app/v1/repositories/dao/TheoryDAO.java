06b672baed5fa998392972187495a8ad8f1e8081
dimochkindl <91985043+dimochkindl@users.noreply.github.com>
2024-02-18 18:35:22 +0300

    added dao's

--- project/src/main/java/app/v1/repositories/dao/TheoryDAO.java
+++ project/src/main/java/app/v1/repositories/dao/TheoryDAO.java
@@ -2,7 +2,6 @@ package app.v1.repositories.dao;
 
 import app.v1.entities.Theory;
 import app.v1.repositories.BaseRepository;
-import com.fasterxml.jackson.databind.ser.Serializers;
 
 public interface TheoryDAO extends BaseRepository<Theory> {
 }
