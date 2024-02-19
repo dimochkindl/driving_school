cbc4d46ec4cc824560eebf4db8840f98ed0ebd4f
dimochkindl <91985043+dimochkindl@users.noreply.github.com>
2024-02-19 04:11:09 +0300

    connector

---
+++ project/src/main/java/app/v1/repositories/DbConnector.java
@@ -0,0 +1,31 @@
+package app.v1.repositories;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnector {
+    public static Connection getConnection(){
+        final String url = "jdbc:postgresql://localhost:5432/driving_school";
+        final String password = "lilasgard228";
+        final String username = "postgres";
+
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            return connection;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void closeConnection(Connection connection){
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
