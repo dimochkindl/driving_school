0b59c1f3da6c975dbb89967ae99e9e2edf7f62e5
dimochkindl <91985043+dimochkindl@users.noreply.github.com>
2024-02-22 21:04:15 +0300

    trying to add mockito

--- project/src/test/java/app/v1/repositories/impl/StudentDAOImlTest.java
+++ project/src/test/java/app/v1/repositories/impl/StudentDAOImlTest.java
@@ -4,107 +4,82 @@ import app.v1.entities.Student;
 import app.v1.repositories.DbConnector;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.MockitoAnnotations;
 
 import java.sql.*;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
 class StudentDAOImlTest {
 
     private StudentDAOIml repo;
+
+    @Mock
     private Connection mockConnection;
+
+    @Mock
     private Statement mockStatement;
+
+    @Mock
     private PreparedStatement mockPreparedStatement;
+
+    @Mock
     private ResultSet mockResultSet;
 
     @BeforeEach
-    void setUp() throws SQLException {
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        try (MockedStatic<DbConnector> mockedStatic = mockStatic(DbConnector.class)) {
+            mockedStatic.when(DbConnector::getConnection).thenReturn(mockConnection);
+        }
+
         repo = new StudentDAOIml();
-
-        // Создаем моки
-        mockConnection = mock(Connection.class);
-        mockStatement = mock(Statement.class);
-        mockPreparedStatement = mock(PreparedStatement.class);
-        mockResultSet = mock(ResultSet.class);
-
-        // Определяем поведение моков
-        when(mockConnection.createStatement()).thenReturn(mockStatement);
-        when(mockConnection.prepareStatement(anyString())).thenReturn(mockPreparedStatement);
-
-        // Моки для DbConnector
-        mockStatic(DbConnector.class);
-        when(DbConnector.getConnection()).thenReturn(mockConnection);
     }
 
     @Test
     void getAll() throws SQLException {
-        // Определяем поведение мока ResultSet
-        when(mockStatement.executeQuery(anyString())).thenReturn(mockResultSet);
-        when(mockResultSet.next()).thenReturn(true, false);
+        prepareMockResultSet(true, false);
 
-        // Ваш тестовый код
         List<Student> students = repo.getAll();
 
-        // Проверки
         assertEquals(1, students.size());
-        for (var student : students) {
-            System.out.println(student);
-        }
+        students.forEach(student -> System.out.println(student));
     }
 
     @Test
     void save() throws SQLException {
-        // Ваш тестовый код
+        prepareMockPreparedStatementAndResultSet(true);
 
-        // Определяем поведение мока PreparedStatement и ResultSet
-        when(mockConnection.prepareStatement(anyString(), anyInt())).thenReturn(mockPreparedStatement);
-        when(mockPreparedStatement.getGeneratedKeys()).thenReturn(mockResultSet);
-        when(mockResultSet.next()).thenReturn(true);
-        when(mockResultSet.getLong(1)).thenReturn(1L);
-
-        // Ваш тестовый код
-        Student student = new Student();
-        student.setName("Mishanya");
-        student.setSurname("Hos");
-        student.setPhone("+4354234");
-        student.setCategory("A");
-
+        Student student = prepareStudent();
         Long id = repo.getLastId();
         student.setId(++id);
         repo.save(student);
 
-        // Проверки
         verify(mockPreparedStatement, times(1)).executeUpdate();
     }
 
     @Test
     void getBySurname() throws SQLException {
-        // Определяем поведение мока ResultSet
-        when(mockPreparedStatement.executeQuery()).thenReturn(mockResultSet);
-        when(mockResultSet.next()).thenReturn(true, false);
+        prepareMockResultSet(true, false);
 
-        // Ваш тестовый код
         String surnameToFind = "Hos";
         List<Student> foundStudent = repo.getBySurname(surnameToFind);
 
-        // Проверки
         assertNotNull(foundStudent);
-        for (var student : foundStudent) {
-            assertEquals(surnameToFind, student.getSurname());
-        }
+        foundStudent.forEach(student -> assertEquals(surnameToFind, student.getSurname()));
     }
 
     @Test
     void delete() throws SQLException {
-        // Ваш тестовый код
+        prepareMockResultSet(true, false);
 
-        // Определяем поведение мока ResultSet
-        when(mockPreparedStatement.executeQuery()).thenReturn(mockResultSet);
-        when(mockResultSet.next()).thenReturn(true, false);
-
-        // Ваш тестовый код
         Long id = repo.getLastId();
         Student studentBeforeDeletion = repo.getById(id);
         assertNotNull(studentBeforeDeletion);
@@ -112,11 +87,34 @@ class StudentDAOImlTest {
 
         repo.delete(id);
 
-        // Проверки
         verify(mockPreparedStatement, times(1)).executeUpdate();
 
         Student studentAfterDeletion = repo.getById(id);
         assertNull(studentAfterDeletion);
         System.out.println("After deletion: " + studentAfterDeletion);
     }
+
+    private void prepareMockResultSet(boolean... nextReturnValues) throws SQLException {
+        when(mockStatement.executeQuery(anyString())).thenReturn(mockResultSet);
+        for (boolean nextReturnValue : nextReturnValues) {
+            when(mockResultSet.next()).thenReturn(nextReturnValue);
+        }
+    }
+
+    private void prepareMockPreparedStatementAndResultSet(boolean nextReturnValue) throws SQLException {
+        when(mockConnection.prepareStatement(anyString(), anyInt())).thenReturn(mockPreparedStatement);
+        when(mockPreparedStatement.getGeneratedKeys()).thenReturn(mockResultSet);
+        when(mockResultSet.next()).thenReturn(nextReturnValue);
+        when(mockResultSet.getLong(1)).thenReturn(1L);
+    }
+
+    private Student prepareStudent() {
+        Student student = new Student();
+        student.setName("Mishanya");
+        student.setSurname("Hos");
+        student.setPhone("+4354234");
+        student.setCategory("A");
+        return student;
+    }
 }
+
